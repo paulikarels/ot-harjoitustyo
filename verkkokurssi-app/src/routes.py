d55a41d143bf24ed55524f4f3da3d485dc66ef96
paulikarels <pauli.karels@gmail.com>
2024-04-09 22:41:34 +0300

    Remove unnecessary file

--- verkkokurssi-app/src/routes.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from repositories.courses import courses
-import exercises
-from flask import render_template, redirect, request, session
-
-
-@app.route('/')
-def index():
-    return render_template("index.html", username=users.username())
