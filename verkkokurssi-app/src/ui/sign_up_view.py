673333179a95c900009f17f2b93f533f2667aca7
paulikarels <pauli.karels@gmail.com>
2024-04-15 19:21:43 +0300

    Create sign up view

--- verkkokurssi-app/src/ui/sign_up_view.py
+++ verkkokurssi-app/src/ui/sign_up_view.py
@@ -1,2 +1,59 @@
 from tkinter import ttk, StringVar, constants
 from services.app_service import app_service
+
+class SignupView:
+    def __init__(self, root, handle_signup, handle_show_login_view):
+        self._root = root
+        self._handle_signup = handle_signup
+        self._handle_show_login_view = handle_show_login_view
+        self._frame = None
+        self._username_var = StringVar()
+        self._password_var = StringVar()
+
+        self._initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+        heading_label = ttk.Label(master=self._frame, text="Sign Up")
+        username_label = ttk.Label(master=self._frame, text="Username")
+        username_entry = ttk.Entry(master=self._frame, textvariable=self._username_var)
+
+        password_label = ttk.Label(master=self._frame, text="Password")
+        password_entry = ttk.Entry(master=self._frame, textvariable=self._password_var)
+
+        signup_button = ttk.Button(
+            master=self._frame,
+            text="Sign Up",
+            command=self._handle_signup
+        )
+
+        login_button = ttk.Button(
+            master=self._frame,
+            text="Back to Login",
+            command=self._handle_show_login_view
+        )
+
+        x=2
+        y=2
+
+        heading_label.grid(columnspan=2, sticky=(constants.E, constants.W), padx=8, pady=8)
+        username_label.grid(padx=x, pady=y)
+        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=x, pady=y)
+        password_label.grid(padx=x, pady=y)
+        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=x, pady=y)
+        signup_button.grid(columnspan=2, sticky=(constants.E, constants.W), padx=x, pady=y)
+        login_button.grid(columnspan=2, sticky=(constants.E, constants.W), padx=x, pady=y)
+
+        self._frame.grid_columnconfigure(1, weight=1, minsize=500)
+
+    def _handle_signup(self):
+        username = self._username_var.get()
+        password = self._password_var.get()
+
+        self._handle_signup(username, password)
--- verkkokurssi-app/src/ui/ui.py
+++ verkkokurssi-app/src/ui/ui.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, ttk, constants
 from ui.login_view import LoginView
+from ui.sign_up_view import SignupView
 
 class UI:
     def __init__(self, root):
@@ -27,7 +28,20 @@ class UI:
         self._current_view.pack()
 
     def _show_handle_sign_up_view(self):
+        self._hide_current_view()
+
+        self._current_view = SignupView(
+            self._root,
+            self._handle_signup,
+            self._show_login_view
+        )
+
+        self._current_view.pack()
+
         pass
 
-    def _handle_login(self):
+    def _handle_login(self, username, password):
+        pass
+
+    def _handle_signup(self, username, password):
         pass
